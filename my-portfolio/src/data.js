78ad9b3c934b524722e6217f15f262e1c9abac9a
Kiran Babu <kiranbabu3355@gmail.com>
2023-10-07 19:16:43 +0530

    Builded

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: "Maveli Portfolio",
+    subtitle: "Bootstrap and Javascript",
+    description:
+      "Portfolio developed for Maveli,as part of Mavelikk-oru-Portfolio Event cinducted my GTech Mulearn",
+    image: "./Maveli.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "K-FLIX",
+    subtitle: "React and OMDB API",
+    description:
+      "A movie search website developed using React and OMDB API, Seach for your favourite movies and get the details of the movie",
+    image: "./K-FLIX.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Tic-Tac-Toe",
+    subtitle: "React",
+    description:
+      "Simple Tic-Tac-Toe Game developed using React, It shows the Winner and also has a reset functionality to start the game form scratch again                                                                                                                                                                                          .",
+    image: "./Tic-Tac-Toe.gif",
+    link: "https://kiran-tic-tac-toe.netlify.app/",
+  },
+  {
+    title: "Digital Clock",
+    subtitle: "HTML,CSS and JS",
+    description:
+      "An Elegant looking Digital Clock developed using HTML,CSS and Js ,with a 24 hour format Click to see the live Time update in the clock",
+    image: "./Clock.gif",
+    link: "https://kiran-digitalclock.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Java","C","JavaScript",
+  "Python","React","HTML","CSS",
+  "Tailwind CSS","Bootstrap",
+  "Git"
+
+];
