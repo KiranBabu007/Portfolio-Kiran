2e97e3e61bbecccdbf14a7836052d922c4d567a6
Kiran Babu <kiranbabu3355@gmail.com>
2024-04-26 00:17:43 +0530

    Update asset paths and project details in index.html and asset-manifest.json

--- src/data.js
+++ src/data.js
@@ -1,27 +1,36 @@
 export const projects = [
   {
-    title: "Curently",
-    subtitle: "React Native + Expo and Firebase",
-    description:
-      "A Hardware + Software Project Which measures the household Energy Consumption and help reduce the electricty bill,",
-    image: "./Maveli.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
+
     title: "Maveli Portfolio",
     subtitle: "Bootstrap and Javascript",
     description:
       "Portfolio developed for Maveli,as part of Mavelikk-oru-Portfolio Event cinducted my GTech Mulearn",
     image: "./Maveli.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://kiranbabu007.github.io/Maveli-Portfolio/",
   },
   {
-    title: "K-FLIX",
-    subtitle: "React and OMDB API",
+    title: "Cyber Odyssey Round 1",
+    subtitle: "React with Shadcn ui",
     description:
-      "A movie search website developed using React and OMDB API, Seach for your favourite movies and get the details of the movie",
-    image: "./K-FLIX.gif",
-    link: "https://reactbootcamp.com",
+      "Round 1 Welcome page of CYber Odyssey, A CTF Event Hosted by us at our College SJCET during Technical Fest Asthra 2k24",
+    image: "./cyber.gif",
+    link: "https://cyber-odyssey.netlify.app/",
+  },
+  {
+    title: "AI Conversation and Image Generation Platform",
+    subtitle: "Next.js and Supabase",
+    description:
+      "Built a website using Next.js and Supabase for Image Generation and Image Generation",
+    image: "./ai.gif",
+    link: "https://genai-x.vercel.app/",
+  },
+  {
+    title: "Curently",
+    subtitle: "React Native + Expo and Firebase",
+    description:
+      "A Hardware + Software Project Which measures the household Energy Consumption and help reduce the electricty bill,",
+    image: "./curently.gif",
+    link: "https://drive.google.com/file/d/1a-6ZnnnmEvt577iuf9rc2nS-jsglSfGu/view?usp=sharing",
   },
   {
     title: "Tic-Tac-Toe",
@@ -31,6 +40,7 @@ export const projects = [
     image: "./Tic-Tac-Toe.gif",
     link: "https://kiran-tic-tac-toe.netlify.app/",
   },
+
   {
     title: "Digital Clock",
     subtitle: "HTML,CSS and JS",
@@ -41,28 +51,11 @@ export const projects = [
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
-
 export const skills = [
   "JavaScript",
-  "Python","C","HTML","CSS",
-  "Tailwind CSS","Bootstrap","React.js",
-  "Next.js","Firebase",
-  "Git","Node.js","Express.js"
+  "Python", "C", "HTML", "CSS",
+  "Tailwind CSS", "Bootstrap", "React.js",
+  "Next.js", "Firebase",
+  "Git", "Node.js", "Express.js","TypeScript","Java","Postman",
 
 ];
